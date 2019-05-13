652da722b1619f740b89a713eb722acc2bf8be3b
HulkSTD <hulk1257@gmail.com>
2019-05-14 03:02:52 +0900

    a

--- Assets/Scripts/CreateCharacter.cs
+++ Assets/Scripts/CreateCharacter.cs
@@ -5,29 +5,118 @@ using UnityEngine.UI;
 
 public class CreateCharacter : MonoBehaviour
 {
+    public static CreateCharacter Instance { get; private set; }
+
     public bool StillHoldOn;
 
     [SerializeField]
-    private Sprite[] CharacterSprites;
+    private List<Sprite> CharacterSprites;
     [SerializeField]
-    private Button[] CharacterButtons;
+    public List<AllyUnit> UnitList;
+    [SerializeField]
+    private Transform SpawnCharacter;
     [SerializeField]
     private GameObject[] Characters;
     [SerializeField]
     private Image MovableObject;
     [SerializeField]
+    private Font font;
     private Transform SpawnPointsParent;
 
     private Color whenSpawn = new Color(1.0f, 1.0f, 1.0f, 0.5f);
     private Color whenNotSpawn = new Color(1.0f, 1.0f, 1.0f, 0.0f);
     private Vector2[] SpawnPoints = new Vector2[5];
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
-        for(int i=0; i < SpawnPointsParent.childCount; ++i)
+        UnitList = new List<AllyUnit>();
+
+        BasicSetting();
+    }
+
+    private void BasicSetting()
+    {
+        SpawnPointsParent = GameObject.Find("SpawnPoints").transform;
+
+        int i;
+
+        for(i=0; i < SpawnPointsParent.childCount; ++i)
         {
             SpawnPoints[i] = SpawnPointsParent.GetChild(i).position;
         }
+
+        if(UnitList.Count == 0)
+        {
+            UnitList.Add(AllyUnit.전사);
+        }
+
+        i = 0;
+
+        while(SpawnCharacter.childCount != i)
+        {
+            Destroy(SpawnCharacter.GetChild(i++).gameObject);
+        }
+
+        UnitList.Sort();
+
+        i = 0;
+
+        foreach(AllyUnit index in UnitList)
+        {
+            CreateSpawnButton(i++, index);
+        }
+    }
+
+    private void CreateSpawnButton(int index, AllyUnit unit)
+    {
+        GameObject gameObject = new GameObject("Unit" + index);
+        GameObject gameObject1 = new GameObject("Cost");
+
+        gameObject.transform.SetParent(SpawnCharacter);
+        gameObject1.transform.SetParent(gameObject.transform);
+
+        Button button = gameObject.AddComponent<Button>();
+        Image image = gameObject.AddComponent<Image>();
+        HoldonEvent holdonEvent = gameObject.AddComponent<HoldonEvent>();
+        CoolDown coolDown = gameObject.AddComponent<CoolDown>();
+        Text costText = gameObject1.AddComponent<Text>();
+
+        image.rectTransform.sizeDelta = new Vector2(150, 150);
+        image.rectTransform.localPosition = new Vector3(-860 + 160 * index, 0, 0);
+        image.rectTransform.localScale = new Vector3(-1, 1, 1);
+
+        costText.rectTransform.sizeDelta = new Vector2(150, 40);
+        costText.rectTransform.localPosition = new Vector3(0, -80);
+        costText.rectTransform.localScale = new Vector3(-1, 1, 1);
+        costText.resizeTextForBestFit = true;
+        costText.resizeTextMaxSize = 300;
+        costText.resizeTextMinSize = 14;
+        costText.font = font;
+        costText.alignment = TextAnchor.MiddleCenter;
+        costText.text = ((int)EnumTransporter.AllyUnitToECost(unit)).ToString();
+        costText.color = Color.yellow;
+
+        image.sprite = CharacterSprites[(int)unit];
+        image.type = Image.Type.Filled;
+        image.fillMethod = Image.FillMethod.Radial360;
+        image.fillOrigin = 0;
+        image.fillAmount = 1;
+        image.fillClockwise = true;
+
+        holdonEvent.CharacterNum = (int)unit;
+        holdonEvent.CoolTime = coolDown;
+        holdonEvent.createCharacter = this;
+
+        coolDown.CostText = costText;
+        coolDown.Image = image;
+        coolDown.Tier = EnumTransporter.AllyUnitToETier(unit);
+        coolDown.Cost = EnumTransporter.AllyUnitToECost(unit);
+        coolDown.CoolOver = true;
     }
 
     public void CreateObject(int Num, CoolDown Object)
@@ -71,4 +160,225 @@ public class CreateCharacter : MonoBehaviour
             Instantiate(Characters[Num], Minvec, new Quaternion());
         }
     }
+}
+
+public class EnumTransporter
+{
+    public static ETier AllyUnitToETier(AllyUnit unit)
+    {
+        int a = (int)unit;
+
+        if(a >= 0 && a <= 10)
+        {
+            return ETier.Tier1;
+        }
+        else if (a >= 11 && a <= 16)
+        {
+            return ETier.Tier2;
+        }
+        else if (a >= 17 && a <= 24)
+        {
+            return ETier.Tier3;
+        }
+
+        return ETier.None;
+    }
+
+    public static ECost AllyUnitToECost(AllyUnit unit)
+    {
+        switch(unit)
+        {
+            case AllyUnit.전사:
+                {
+                    return ECost.Warrior;
+                }
+                
+            case AllyUnit.쌍검사:
+                {
+                    return ECost.SSanggumsa;
+                }
+                
+            case AllyUnit.무사:
+                {
+                    return ECost.Musa;
+                }
+                
+            case AllyUnit.창병:
+                {
+                    return ECost.ChangBeong;
+                }
+
+            case AllyUnit.장창병:
+                {
+                    return ECost.JangChangBeong;
+                }
+
+            case AllyUnit.투창병:
+                {
+                    return ECost.TuChangBeong;
+                }
+
+            case AllyUnit.기마병:
+                {
+                    return ECost.GiMaBeong;
+                }
+
+            case AllyUnit.경기병:
+                {
+                    return ECost.GeongGiBeong;
+                }
+
+            case AllyUnit.기사:
+                {
+                    return ECost.GiSa;
+                }
+
+            case AllyUnit.해머병:
+                {
+                    return ECost.HammerBeong;
+                }
+
+            case AllyUnit.철퇴병:
+                {
+                    return ECost.凸TeauBeong;
+                }
+
+            case AllyUnit.방패병:
+                {
+                    return ECost.BangPeBeong;
+                }
+
+            case AllyUnit.기갑병:
+                {
+                    return ECost.GiGabBeong;
+                }
+
+            case AllyUnit.사제:
+                {
+                    return ECost.Trash;
+                }
+
+            case AllyUnit.고위사제:
+                {
+                    return ECost.GoWiiTrash;
+                }
+
+            case AllyUnit.암살자:
+                {
+                    return ECost.AmSalJa;
+                }
+
+            case AllyUnit.어쎄신:
+                {
+                    return ECost.Assesin;
+                }
+
+            case AllyUnit.귀족:
+                {
+                    return ECost.GuiJok;
+                }
+
+            case AllyUnit.국왕:
+                {
+                    return ECost.GukWang;
+                }
+
+            case AllyUnit.성기사:
+                {
+                    return ECost.SeongGiSa;
+                }
+
+            case AllyUnit.영웅:
+                {
+                    return ECost.AU;
+                }
+
+            case AllyUnit.대장군:
+                {
+                    return ECost.DaeJangGun;
+                }
+
+            case AllyUnit.총사령관:
+                {
+                    return ECost.GunSaRyeongGuan;
+                }
+        }
+        return ECost.None;
+    }
+}
+
+
+public enum ETier : int
+{
+    Tier1 = 3,
+    Tier2 = 10,
+    Tier3 = 20,
+    None = -1
+}
+
+public enum ECost : int
+{
+    Warrior = 5,
+    SSanggumsa = 5,
+    Musa = 8,
+    ChangBeong = 7,
+    JangChangBeong = 7,
+    TuChangBeong = 10,
+    GiMaBeong = 10,
+    GeongGiBeong = 10,
+    JungGiBeong = 10,
+    GiSa = 17,
+    HammerBeong = 20,
+    凸TeauBeong = 25,
+    BangPeBeong = 15,
+    GiGabBeong = 20,
+    Trash = 20,
+    GoWiiTrash = 30,
+    AmSalJa = 30,
+    Assesin = 40,
+    GuiJok = 30,
+    GukWang = 30,
+    SeongGiSa = 40,
+    AU = 50,
+    DaeJangGun = 20,
+    GunSaRyeongGuan = 20,
+    None = -1
+}
+
+public enum ESkill : int
+{
+    SaGiJeungJin = 20,
+    AmSal = 10,
+    HongSu = 30,
+    PokDong = 20,
+    None = -1
+}
+
+public enum AllyUnit : short
+{
+    전사 = 0,
+    쌍검사 = 1,
+    무사 = 3,
+    창병 = 4,
+    장창병 = 5,
+    투창병 = 6,
+    기마병 = 7,
+    경기병 = 8,
+    중기병 = 9,
+    기사 = 10,
+    해머병 = 11,
+    철퇴병 = 12,
+    방패병 = 13,
+    기갑병 = 14,
+    사제 = 15,
+    고위사제 = 16,
+    암살자 = 17,
+    어쎄신 = 18,
+    귀족 = 19,
+    국왕 = 20,
+    성기사 = 21,
+    영웅 = 22,
+    대장군 = 23,
+    총사령관 = 24,
+    아무것도아님 = -1
 }

ba49f0b9bd19b7fa45fe2feddb682ea23a5a117b
Original-Kimchi <k3409912@naver.com>
2019-05-29 17:01:40 +0900

    push 재호싫어

---
+++ Assets/Game/EnemyUnits/Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies
+{
+    GameObject Type;
+    int Count;
+
+//    Enemies(Types types, intCount)
+}
+public enum{
+
+}
+
+public class LevelManager : MonoBehaviour
+{
+    public Transform[] points; // enemy가 생성될 Points
+    public List<Enemies> enemies; // enemy 프리팹
+    public int enemyNum; // 몇 명의 enemy가 나오는지
+
+    private void Start()
+    {
+        StartCoroutine(CreateEnemy());
+    }
+
+
+    IEnumerator CreateEnemy()
+    {
+        while (enemyNum > 0)
+        {
+            int idx = Random.Range(0, points.Length);
+
+            Instantiate(enemies[0], points[idx]);
+            //Debug.Log("Enemy created on" + points[idx].name);
+            enemyNum--;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+}

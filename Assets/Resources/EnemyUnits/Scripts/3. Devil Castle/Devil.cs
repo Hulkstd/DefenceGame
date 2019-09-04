e5aae2f1c3aaad9c592b646cfc3daa8b0b12ee89
Original-Kimchi <39785268+Original-Kimchi@users.noreply.github.com>
2019-09-04 19:51:38 +0900

    devil 왓넝

---
+++ Assets/Resources/EnemyUnits/Scripts/3. Devil Castle/Devil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Devil : Unit
+{
+    public BossPassive bossPassive;
+
+    private void Awake()
+    {
+        DefaultSetting();
+    }
+
+    private void Update()
+    {
+        Loop();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TriggerEnter(collision, "AR");
+    }
+
+    private void OnDisable()
+    {
+        bossPassive.Recovered();
+    }
+}
